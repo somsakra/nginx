d9cb042139c8a187f857d35636db4cff73e8a37b
Somsakra <somsakra@live.com>
2021-08-10 09:09:12 +0700

    first commit

---
+++ ssl/gen.sh
@@ -0,0 +1,7 @@
+openssl genrsa -out server.key 2048
+openssl rsa -in server.key -out server.key
+openssl req -sha256 -new -key server.key -out server.csr -subj '/CN=localhost'
+openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+
+## Combine to .pem
+cat server.crt server.key > cert.pem
